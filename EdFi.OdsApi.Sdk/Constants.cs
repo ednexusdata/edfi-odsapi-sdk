c7d5d037bd72822068894dcb96ecc0ee3d3daf5b
Makoa Jacobsen <makoa@makoajacobsen.com>
2025-06-15 23:17:57 -0700

    Add versions of API

---
+++ EdFi.OdsApi.Sdk/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace EdFi.OdsApi.Sdk;
+
+public class Constants
+{
+    public Dictionary<string, string> Versions = new Dictionary<string, string>()
+    {
+        { "EDFI_ODSAPI_SDK_V54", "v54" },
+        { "EDFI_ODSAPI_SDK_V62", "v62" },
+        { "EDFI_ODSAPI_SDK_V71", "v71" },
+        { "EDFI_ODSAPI_SDK_V72", "v72" },
+        { "EDFI_ODSAPI_SDK_V73", "v73" }
+    };
+
+    public const string EDFI_ODSAPI_SDK_V54 = "EdFi.OdsApi.Sdk.v54";
+    public const string EDFI_ODSAPI_SDK_V62 = "EdFi.OdsApi.Sdk.v62";
+    public const string EDFI_ODSAPI_SDK_V71 = "EdFi.OdsApi.Sdk.v71";
+    public const string EDFI_ODSAPI_SDK_V72 = "EdFi.OdsApi.Sdk.v72";
+    public const string EDFI_ODSAPI_SDK_V73 = "EdFi.OdsApi.Sdk.v73";
+}
