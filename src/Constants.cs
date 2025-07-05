80324572cbd3e954d66942a2666ba7428d6e183c
Makoa Jacobsen <makoa@makoajacobsen.com>
2025-07-06 06:25:27 +0800

    Align version constants with keys

--- src/Constants.cs
+++ src/Constants.cs
@@ -4,11 +4,11 @@ public class Constants
 {
     public static Dictionary<string, string> Versions = new Dictionary<string, string>()
     {
-        { "EDFI_ODSAPI_SDK_V54", "v5.4" },
-        { "EDFI_ODSAPI_SDK_V62", "v6.2" },
-        { "EDFI_ODSAPI_SDK_V71", "v7.1" },
-        { "EDFI_ODSAPI_SDK_V72", "v7.2" },
-        { "EDFI_ODSAPI_SDK_V73", "v7.3" }
+        { EDFI_ODSAPI_SDK_V54, "v5.4" },
+        { EDFI_ODSAPI_SDK_V62, "v6.2" },
+        { EDFI_ODSAPI_SDK_V71, "v7.1" },
+        { EDFI_ODSAPI_SDK_V72, "v7.2" },
+        { EDFI_ODSAPI_SDK_V73, "v7.3" }
     };
 
     public const string EDFI_ODSAPI_SDK_V54 = "EdFi.OdsApi.Sdk.v54";
@@ -18,7 +18,7 @@ public class Constants
     public const string EDFI_ODSAPI_SDK_V73 = "EdFi.OdsApi.Sdk.v73";
 
     public const string EDFI_ODSAPI_V54 = "5.4.0";
-    public const string EDFI_ODSAPI_V64 = "6.4.0";
+    public const string EDFI_ODSAPI_V62 = "6.2.0";
     public const string EDFI_ODSAPI_V71 = "7.1.0";
     public const string EDFI_ODSAPI_V72 = "7.2.0";
     public const string EDFI_ODSAPI_V73 = "7.3.0";
